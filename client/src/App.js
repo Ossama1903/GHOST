2d9958940f2733ed2799c8a93c3a405588b60a4d
Ossama1903 <sheikhusamabilal@gmail.com>
2022-11-22 03:51:33 +0500

    Sidebar gui created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Driver from "./pages/driver/Driver";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      Project Ghost - Initiated
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="drivers">
+              <Route index element={<List />}></Route>
+              <Route path=":driverId" element={<Driver />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
