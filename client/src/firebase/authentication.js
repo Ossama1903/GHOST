8e569eae21ef4aa47f9472f2266fc9269f7fa7e3
Ossama1903 <sheikhusamabilal@gmail.com>
2023-01-06 20:03:49 +0500

    Routes protected, authentication and authorization is now complete

--- client/src/firebase/authentication.js
+++ client/src/firebase/authentication.js
@@ -25,22 +25,24 @@ class Authentication {
 
   async attemptSignIn(email, password) {
     try {
-      await signInWithEmailAndPassword(
+      const user = await signInWithEmailAndPassword(
         this.authenticationInstance,
         email,
         password
       );
+      localStorage.setItem("token", user.user.accessToken);
     } catch (e) {
       console.log(e.code);
       console.log(e.message);
     }
   }
 
-  signInAdmin(email, password) {
+  signInAdmin(email, password, callback) {
     database.getUserByEmail(email, async (user) => {
       if (user) {
         if (user.role === "superadmin" || user.role === "admin") {
           await this.attemptSignIn(email, password);
+          typeof callback === "function" && callback();
         } else {
           console.log(
             "ERROR: User doesn't have permission to log into this portal"
@@ -52,8 +54,11 @@ class Authentication {
     });
   }
 
-  signOutAdmin() {
-    signOut(auth);
+  signOutAdmin(callback) {
+    signOut(auth).then(() => {
+      localStorage.removeItem("token");
+      typeof callback === "function" && callback();
+    });
   }
 
   getCurrentUser() {
